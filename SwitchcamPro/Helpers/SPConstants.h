bc67f2cbdb27ad036c4ae37703701d7eddc5d686
Matt Ketterer <wmketterer@gmail.com>
2012-12-15 18:13:50 -0800

    Adding a helper #define for generating colors

--- SwitchcamPro/Helpers/SPConstants.h
+++ SwitchcamPro/Helpers/SPConstants.h
@@ -16,7 +16,10 @@
 // Constants for the Bucket and Object name.
 #define kAWS_VIDEO_BUCKET         @"upload-switchcam-ios"
 
-//
-
+// User Defaults
 #define kSPUserFacebookIdKey @"SPUserFacebookIdKey"
 #define kSPUserFacebookTokenKey @"SPUserFacebookTokenKey"
+
+// Color Ease
+#define RGBA(r,g,b,a) [UIColor colorWithRed: r/255.0f green: g/255.0f \
+blue: b/255.0f alpha: a]
